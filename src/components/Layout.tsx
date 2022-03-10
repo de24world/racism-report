da5230c6c1fd3fc0139c9b16994cad20e10cc66c
Lee <de24world@gmail.com>
2022-03-10 01:27:45 +0100

    sass, tailwind, postcss, prettier & basic components update

---
+++ src/components/Layout.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from 'next/head';
+import Footer from './Footer';
+import Header from './Header';
+
+type Props = {
+  children: any;
+};
+
+function Layout({ children }: Props) {
+  return (
+    <>
+      <Head>
+        <title>Racism Report App</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <Header />
+
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
