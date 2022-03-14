1f2a88a95ad8ba6746a002358bb64a8275e05859
Lee <de24world@gmail.com>
2022-03-14 09:43:31 +0100

    add auth firebase

---
+++ pages/signup.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import Register from '../src/components/Register';
+
+const SignupPage: NextPage = () => {
+  return (
+    <>
+      Signup Page
+      <Register />
+    </>
+  );
+};
+
+export default SignupPage;
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import { Form, Input, Button, Checkbox } from 'antd';
 import { auth } from '../../firebase';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 type Props = {};
 
 function Login({}: Props) {
-  console.log(auth, 'auth with Firebase');
+  // console.log(auth, 'auth with Firebase');
   //   const userData = createUserWithEmailAndPassword(auth, email, password)
   //     .then((userCredential) => {
   //       // Signed in
@@ -20,44 +21,35 @@ function Login({}: Props) {
   //     });
 
   const onFinish = (values: any) => {
-    console.log('Success:', values);
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
+    console.log('Received values of form: ', values);
   };
 
   return (
     <>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
-          <Input />
+      <Form name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={onFinish}>
+        <Form.Item name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
+          <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
         </Form.Item>
 
-        <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
           </Button>
+          Or <a href="">register now!</a>
         </Form.Item>
       </Form>
-      <Button type="primary">Email Button</Button>
-      <Button type="primary">Gmail Button</Button>
     </>
   );
 }
