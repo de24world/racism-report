0f36c16b00495968dd038e3c302d246c7c902bfe
Lee <de24world@gmail.com>
2022-05-27 02:04:08 +0200

    arrary map update

---
+++ shared/model/people.ts
@@ -0,0 +1,1 @@
+export const People = ['Black Mann', 'Black Women', 'White Mann', 'White Women', 'Yellow Mann', 'Yellow Women'];
--- src/components/CreateForm.tsx
+++ src/components/CreateForm.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import moment, { now } from 'moment';
+import moment from 'moment';
 
 import { getDatabase, ref, set, push } from 'firebase/database';
 import { IDataValue } from '../interface/dataInterface';
 
-import { Form, Input, Button, Radio, Select, DatePicker, message } from 'antd';
+import { Form, Input, Button, Radio, Select, DatePicker, message, Tooltip } from 'antd';
+import { InfoCircleOutlined } from '@ant-design/icons';
 import { IUser } from '../interface/dataInterface';
 import { Countries } from '../../shared/model/country';
+import { People } from '../../shared/model/people';
 import ReactCountryFlag from 'react-country-flag';
 type Props = {
   user: IUser;
@@ -35,12 +37,21 @@ const CreateForm = ({ user }: Props) => {
     rules: [{ type: 'object' as const, required: true, message: 'Please select time!' }],
   };
 
+  const peopleArray = (
+    <>
+      {People.map((peoples, peopleIndex) => (
+        <Select.Option key={peopleIndex} value={peoples}>
+          {peoples}
+        </Select.Option>
+      ))}
+    </>
+  );
+
   const onFinish = (values: IDataValue) => {
     // e.preventDefault();
 
     const db = getDatabase();
     const postListRef = ref(db, `reportDB/${Date.now()}`);
-    // const newPostRef = push(postListRef);
     set(postListRef, {
       useremail: values.useremail,
       offender: values.offender,
@@ -64,7 +75,7 @@ const CreateForm = ({ user }: Props) => {
       <Form
         form={form}
         {...layout}
-        name="nest-messages"
+        name="create-report"
         initialValues={{
           useremail: user.email,
           submitTime: Date(),
@@ -77,52 +88,35 @@ const CreateForm = ({ user }: Props) => {
         </Form.Item>
         <Form.Item name="offender" label="offender" rules={[{ required: true, message: 'Please select offender!' }]}>
           <Select showSearch placeholder="Select to Offender">
-            <Select.Option value="Black Mann">Black Mann</Select.Option>
-            <Select.Option value="Black Women">Black Women</Select.Option>
-            <Select.Option value="White Mann">White Mann</Select.Option>
-            <Select.Option value="White Women">White Women</Select.Option>
-            <Select.Option value="Yellow Mann">Yellow Mann</Select.Option>
-            <Select.Option value="Yellow Women">Yellow Women</Select.Option>
+            {peopleArray}
           </Select>
         </Form.Item>
         <Form.Item name="victim" label="Victim" rules={[{ required: true, message: 'Please select Victim!' }]}>
           <Select showSearch placeholder="Select to Victim">
-            <Select.Option value="Black Mann">Black Mann</Select.Option>
-            <Select.Option value="Black Women">Black Women</Select.Option>
-            <Select.Option value="White Mann">White Mann</Select.Option>
-            <Select.Option value="White Women">White Women</Select.Option>
-            <Select.Option value="Yellow Mann">Yellow Mann</Select.Option>
-            <Select.Option value="Yellow Women">Yellow Women</Select.Option>
+            {peopleArray}
           </Select>
         </Form.Item>
         *만약 온라인이면 가해자 국가로 선택하세요
         <Form.Item name="place" label="place" hasFeedback rules={[{ required: true, message: 'Please select your country!' }]}>
-          <Select
-            // showSearch
-            // optionFilterProp="children"
-            // filterOption={(input, option) => option.children.includes(input)}
-            // filterSort={(optionA, optionB) => optionA.children.toLowerCase().localeCompare(optionB.children.toLowerCase())}
-            placeholder="please select place"
-          >
-            {Countries.map((item, countryIndex) => (
-              <Select.Option key={countryIndex} value={item.name}>
+          <Select showSearch placeholder="please select place">
+            {Countries.map((countries, countryIndex) => (
+              <Select.Option key={countryIndex} value={countries.name}>
                 <ReactCountryFlag
-                  countryCode={item.code}
+                  countryCode={countries.code}
                   svg
                   cdnUrl="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.4.3/flags/1x1/"
                   cdnSuffix="svg"
-                  title={item.name}
+                  title={countries.name}
                   // style={{
                   //   width: '2em',
                   //   height: '2em',
                   // }}
                 />
-                {item.name}
+                {countries.name}
               </Select.Option>
             ))}
           </Select>
         </Form.Item>
-        {/* AutoComplete Update */}
         <Form.Item
           name="evidence"
           label="Evidence"
@@ -140,7 +134,19 @@ const CreateForm = ({ user }: Props) => {
             },
           ]}
         >
-          <Input placeholder="input placeholder" />
+          <Input
+            placeholder="Youtube URL"
+            // addonBefore="https://youtu.be/"
+            suffix={
+              <Tooltip title="유튜브 URL 예: https://youtu.be/HOLc4S2N5eI">
+                <InfoCircleOutlined
+                  style={{
+                    color: 'rgba(0,0,0,.45)',
+                  }}
+                />
+              </Tooltip>
+            }
+          />
         </Form.Item>
         <Form.Item name="occurDate" label="날짜" {...dataConfig}>
           <DatePicker
@@ -149,14 +155,16 @@ const CreateForm = ({ user }: Props) => {
               return moment() <= current;
             }}
           />
-          {/* var unixTimestamp = moment('2012.08.10', 'YYYY.MM.DD').unix(); */}
         </Form.Item>
         <Form.Item label="level" name="level" rules={[{ required: true, message: 'Please select level!' }]}>
           <Radio.Group>
-            <Radio.Button value="1">1단계:언어</Radio.Button>
-            <Radio.Button value="2">2단계:신체적</Radio.Button>
-            <Radio.Button value="3">3단계:강력범죄</Radio.Button>
+            <Radio.Button value="1">1단계</Radio.Button>
+            <Radio.Button value="2">2단계</Radio.Button>
+            <Radio.Button value="3">3단계</Radio.Button>
           </Radio.Group>
+          <Tooltip title="1단계는 언어, 행동 / 2단계는 폭력 / 3단계는 살인, 성폭행 등 강력범죄" color="blue">
+            <InfoCircleOutlined style={{ fontSize: '1rem', paddingLeft: '0.5rem', verticalAlign: 'top', color: '#08c' }} />
+          </Tooltip>
         </Form.Item>
         <Form.Item name="description" label="설명">
           <Input />
@@ -164,7 +172,7 @@ const CreateForm = ({ user }: Props) => {
         <Form.Item label="제출냘짜" name="submitTime" style={{ display: 'none' }}>
           제출날짜 = submitTime / 안보임
         </Form.Item>
-        추가작업할 것: place 소문자 검색가능 / icon표시로 설명하기 / item name 에러/ 캡쳐?
+        추가작업할 것: icon표시로 설명하기 / item name 에러/ 캡쳐?
         <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
           <Button type="primary" htmlType="submit">
             Submit
