a7f293edb7d1c04eeab010173470af1ce0577aef
Lee <de24world@gmail.com>
2022-06-06 00:04:46 +0200

    video dynmaic page update

---
+++ pages/video/[id].tsx
@@ -0,0 +1,36 @@
+import { useRouter } from 'next/router';
+import { GetServerSideProps } from 'next';
+
+import { ref, child, get } from 'firebase/database';
+
+import { Table } from 'antd';
+import type { ColumnsType, TableProps } from 'antd/lib/table';
+
+interface Props {
+  post: any;
+}
+
+const ListPage = ({ post }: Props) => {
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(post, 'post??');
+  return (
+    <>
+      Dynamic ID Page <br />
+      <p>id: {id}</p>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL}/reportDB/${context.params.id}.json`);
+  const post = await res.json();
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
+
+export default ListPage;
--- pages/video/index.tsx
+++ pages/video/index.tsx
@@ -3,8 +3,9 @@ import Head from 'next/head';
 import { useState, Fragment } from 'react';
 import { Typography, Input, Checkbox } from 'antd';
 import { VideoCameraOutlined } from '@ant-design/icons';
-import VideoList from '../src/components/VideoList';
-import { IDataProps } from '../src/interface/dataInterface';
+import VideoList from '../../src/components/VideoList';
+import { IDataProps } from '../../src/interface/dataInterface';
+import { Value } from 'sass';
 
 interface Props {
   data: IDataProps[];
@@ -18,7 +19,7 @@ const VideoPage = function ({ data }: Props) {
 
   // serach & filter
   const [query, setQuery] = useState('');
-  const [searchDataKeys, setSearchDataKeys] = useState(['country', 'city']);
+  const [searchDataKeys, setSearchDataKeys] = useState(['country']);
   // const dataKeys = reportData[0] && Object.keys(reportData[0]);
   // console.log(dataKeys, 'test : dateKeys??');
 
@@ -29,6 +30,15 @@ const VideoPage = function ({ data }: Props) {
   //   );
   // }
 
+  // const dataKeys = Object.keys(data);
+  // console.log(dataKeys, 'test : dataKeys in video Page');
+
+  // const dataEntries = Object.entries(data);
+  // console.log(dataEntries, 'test :dataEntries in video Page');
+
+  // const dataValues1 = Object.values(data);
+  // console.log(dataValues1, 'test : dataValues1 in video Page');
+
   return (
     <>
       <Head>
@@ -38,13 +48,23 @@ const VideoPage = function ({ data }: Props) {
 
       <Title level={2}>
         <VideoCameraOutlined className="pr-4" />
-        Video Page
+        Video Page / 해야할 것 : 카드로 만들기, 검색 및 필터, 페이지 넘기기
       </Title>
       <Paragraph>해당 페이지는 비디오 페이지입니다. 인종차별과 관련한 많은 영상들을 볼 수 있습니다. 또한 비디오를 검색 및 필터할 수 있습니다.</Paragraph>
 
       <Input type="text" placeholder="검색할 값을 입력하세요" value={query} onChange={(e) => setQuery(e.target.value)} />
+      {/* <Input type="text" placeholder="검색할 값을 입력하세요" value={query} onChange={(e) => setQuery(e.target.value)} /> */}
 
       <div className="md:flex flex-row">
+        <ul className="grid md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5">
+          {Object.keys(data).map((createdKey, dataIndex) => (
+            <li className="p-1.5" key={dataIndex}>
+              dataIndex : {dataIndex} <br />
+              createdKey : {createdKey} <br />
+              {/* data[dataKey].evidence : {data[dataKey].evidence} */}
+            </li>
+          ))}
+        </ul>
         {/* {dataKeys &&
           dataKeys.map((dataKey, index) => (
             <Fragment key={index}>
@@ -61,7 +81,7 @@ const VideoPage = function ({ data }: Props) {
           ))} */}
       </div>
 
-      <VideoList data={reportData} />
+      {/* <VideoList data={reportData} /> */}
 
       {/* <VideoList data={searchData(reportData)} /> */}
     </>
