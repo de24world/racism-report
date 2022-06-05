9658db0faa52b0bf38f0d53cec471b859763cd48
Lee <de24world@gmail.com>
2022-06-05 02:24:24 +0200

    antd list table sample update

--- pages/list.tsx
+++ pages/list.tsx
@@ -3,16 +3,121 @@ import { realtimeDB } from '../firebase';
 import TableList from '../src/components/TableList';
 import { ref, child, get } from 'firebase/database';
 
-interface Props {
-  database: any;
+import { Table } from 'antd';
+import type { ColumnsType, TableProps } from 'antd/lib/table';
+
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
 }
 
-const ListPage = ({ database }: Props) => {
-  console.log(database, 'test : database');
+const ListPage = ({ reportData }: DataType) => {
+  // console.log(reportData, 'test : data in List');
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      filters: [
+        {
+          text: 'Joe',
+          value: 'Joe',
+        },
+        {
+          text: 'Category 1',
+          value: 'Category 1',
+          children: [
+            {
+              text: 'Yellow',
+              value: 'Yellow',
+            },
+            {
+              text: 'Pink',
+              value: 'Pink',
+            },
+          ],
+        },
+        {
+          text: 'Category 2',
+          value: 'Category 2',
+          children: [
+            {
+              text: 'Green',
+              value: 'Green',
+            },
+            {
+              text: 'Black',
+              value: 'Black',
+            },
+          ],
+        },
+      ],
+      filterMode: 'tree',
+      filterSearch: true,
+      onFilter: (value: string, record) => record.name.includes(value),
+      width: '30%',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      sorter: (a, b) => a.age - b.age,
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      filters: [
+        {
+          text: 'London',
+          value: 'London',
+        },
+        {
+          text: 'New York',
+          value: 'New York',
+        },
+      ],
+      onFilter: (value: string, record) => record.address.startsWith(value),
+      filterSearch: true,
+      width: '40%',
+    },
+  ];
+
+  const data: DataType[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+    },
+    {
+      key: '4',
+      name: 'Jim Red',
+      age: 32,
+      address: 'London No. 2 Lake Park',
+    },
+  ];
+
+  const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+    console.log('params', pagination, filters, sorter, extra);
+  };
+
   return (
     <>
       List Page
-      <TableList />
+      <Table columns={columns} dataSource={data} onChange={onChange} />;{/* <TableList /> */}
     </>
   );
 };
@@ -20,24 +125,12 @@ const ListPage = ({ database }: Props) => {
 // https://firebase.google.com/docs/database/web/read-and-write#read_data
 // https://ashleemboyer.com/blog/nextjs-firebase-blog-03
 export async function getStaticProps({}) {
-  const data = await res.json();
-  // const dbRef = ref(realtimeDB);
-  // get(child(dbRef, `reportDB`))
-  //   .then((snapshot) => {
-  //     if (snapshot.exists()) {
-  //       console.log(snapshot.val(), 'snapshot.val() in realtimeDB!!!');
-  //     } else {
-  //       console.log('No data available in realTimeDB');
-  //     }
-  //   })
-  //   .catch((error) => {
-  //     console.error(error);
-  //   });
+  const res = await fetch(`${process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL}/reportDB.json`);
+  const reportData = await res.json();
 
-  // const database = JSON.stringify(dbRef);
   return {
     props: {
-      database,
+      reportData,
       // ...(await serverSideTranslations(locale, ['common'])),
     },
   };
