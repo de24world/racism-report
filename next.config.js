533ca4d1969bfd837670e78fb808bd8aac7c1745
Lee <de24world@gmail.com>
2022-06-13 17:22:56 +0200

    next.js config type check turn off

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const nextConfig = {
     loader: 'imgix',
     path: 'https://noop/',
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
