66028b6635aa04e0cb859f5f51b8fc6e9e54a12e
Lee <de24world@gmail.com>
2022-06-19 00:25:37 +0200

    todolist add

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,9 @@ const Home: NextPage = () => {
     <>
       <h1 className="text-red-600">Index(Home) Page</h1>
       <h3>해야할 것</h3>
+      <p>renovate</p>
+      <p>Devops Eslint(next.js config typescript 에러 체크 활성화) 등</p>
+
       <p>관리자 삭제</p>
       <p>쿠키 및 세션 설정</p>
       <p>차트</p>
